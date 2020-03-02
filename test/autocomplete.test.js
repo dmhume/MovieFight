f1020d07b3f30dd2eb1a8438da5f54f1391fb448
dmhume <davidhumecodes@gmail.com>
2020-03-02 10:20:07 -0500

    Add files via upload
    added testing programs for movie fight project

---
+++ test/autocomplete.test.js
@@ -0,0 +1,51 @@
+const wairFor = selector => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Whiplash" },
+        { Title: "The Lighthouse" }
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
+});
+
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching, dropdown opens up", async () => {
+  const input = document.querySelector("input");
+  input.value = "the lighthouse";
+  input.dispatchEvent(new Event("input"));
+
+  await wairFor(".dropdown-item");
+
+  const items = document.querySelectorAll(".dropdown-item");
+
+  expect(items.length).to.equal(3);
+});
